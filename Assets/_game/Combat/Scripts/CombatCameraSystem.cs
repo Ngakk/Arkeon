4bd890c1870efcc68084cba2fb97e3b09b529bb8
Galaxyben <galaxyben1@gmail.com>
2019-03-21 14:52:06 -0600

    CineMachine Basics & Camera Positions

---
+++ Assets/_game/Combat/Scripts/CombatCameraSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos
+{
+    public enum CameraPositions
+    {
+        COMBAT,
+        PLAYER_AND_INV,
+        INV_1,
+        INV_2,
+        INV_3
+    }
+
+    public class CombatCameraSystem : MonoBehaviour
+    {
+        public GameObject mainCam;
+        public GameObject combatPos;
+        public GameObject playerSetsPos;
+        public GameObject invocationPos1;
+        public GameObject invocationPos2;
+        public GameObject invocationPos3;
+
+        void Start()
+        {
+
+        }
+        
+        void Update()
+        {
+
+        }
+    }
+}
