e42aa8a9a5b04d6710a6f3dbef254c9187456ff8
Ngakk <32682022+Ngakk@users.noreply.github.com>
2019-03-26 15:02:29 -0600

    Creando escena y primeros scripts

---
+++ Assets/_game/Protoype Combat Gameplay/Scripts/ArkeonAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArkeonAttack : MonoBehaviour
+{
+    /// <summary>
+    /// What to do before the hit. Examples are incrementing stats
+    /// </summary>
+    public void PreHit()
+    {
+
+    }
+
+    /// <summary>
+    /// What to do when attack hits.
+    /// </summary>
+    public void OnHit()
+    {
+        
+    }
+
+    public void PostHit()
+    {
+
+    }
+
+    /// <summary>
+    /// Regresa el daño que se va hacer usando el ... TODO
+    /// </summary>
+    /// <param name="_atk"></param>
+    /// <param name="_pwr"></param>
+    /// <param name="_def"></param>
+    /// <returns></returns>
+    private int CalculateDamage(int _atk, int _pwr, int _def)
+    {
+        return (int)Mathf.Round((_atk * _pwr) / _def);
+    }
+}
---
+++ Assets/_game/Protoype Combat Gameplay/Scripts/ArkeonStats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArkeonStats : ScriptableObject
+{
+    public int HP;
+    public int MaxHP;
+    public int Atk;
+    public int Def;
+    
+    public List<>
+}
