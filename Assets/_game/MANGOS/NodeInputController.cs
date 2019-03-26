d2232ef1d1b20e3e124f43d8688606339523eb43
Rubén Ostos <reflejodevapor@gmail.com>
2019-03-26 15:11:21 -0600

    some changes
    Added working buttons

--- Assets/_game/MANGOS/NodeInputController.cs
+++ Assets/_game/MANGOS/NodeInputController.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class NodeInputController : MonoBehaviour
 {
+
+    public Stack stack;
+    public GameObject[] touchbuttons;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AddToStack(string button)
+    {
+        Debug.Log(button);
     }
 
 
