16f47cd8b009f891d80c2e1ec8475cdb7ed567e7
Galaxyben <galaxyben1@gmail.com>
2019-04-02 13:41:02 -0600

    Posicionamiento de cámaras y eventos para su manejo

--- Assets/_game/Combat/Scripts/CombatCameraSystem.cs
+++ Assets/_game/Combat/Scripts/CombatCameraSystem.cs
@@ -3,39 +3,103 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using UnityEngine.Timeline;
+using UnityEngine.Playables;
 
 namespace Mangos
 {
-    public enum CameraPositions
-    {
-        COMBAT,
-        PLAYER_AND_INV,
-        INV_1,
-        INV_2,
-        INV_3
-    }
 
     public class CombatCameraSystem : MonoBehaviour
     {
-        public CameraPositions CameraPositions;
-        public List<TimelineAsset> timelines;
-        public GameObject combatPos;
-        public GameObject playerPos;
+        public List<PlayableDirector> playableDirectors;
+        public GameObject enemy;
+        public GameObject player;
         public GameObject invocationPos1;
         public GameObject invocationPos2;
         public GameObject invocationPos3;
+        public GameObject familiar;
 
         public CinemachineVirtualCamera[] CM_VCameras;
-        public GameObject currentObjective;
 
         void Start()
         {
-            CameraPositions = CameraPositions.COMBAT;
+            playableDirectors[0].Play();
+            Debug.Log(playableDirectors[3].duration);
         }
-        
-        void Update()
-        {
 
+        public void CombatView()
+        {
+            playableDirectors[1].Stop();
+            playableDirectors[3].Play();
+            StartCoroutine("FinishCombatViewFromPlayer");
+        }
+
+        public void ChangeViewToPlayerInv()
+        {
+            playableDirectors[0].Stop();
+            playableDirectors[2].Play();
+            StartCoroutine("FinishPlayerViewFromCombat");
+        }
+
+        public void FocusPlayer()
+        {
+            for (int i = 0; i < CM_VCameras.Length; i++)
+            {
+                CM_VCameras[i].LookAt = player.transform;
+            }
+        }
+
+        public void FocusInv1()
+        {
+            for (int i = 0; i < CM_VCameras.Length; i++)
+            {
+                CM_VCameras[i].LookAt = invocationPos1.transform;
+            }
+        }
+
+        public void FocusInv2()
+        {
+            for (int i = 0; i < CM_VCameras.Length; i++)
+            {
+                CM_VCameras[i].LookAt = invocationPos2.transform;
+            }
+        }
+
+        public void FocusInv3()
+        {
+            for (int i = 0; i < CM_VCameras.Length; i++)
+            {
+                CM_VCameras[i].LookAt = invocationPos3.transform;
+            }
+        }
+
+        public void FocusFamiliar()
+        {
+            for (int i = 0; i < CM_VCameras.Length; i++)
+            {
+                CM_VCameras[i].LookAt = familiar.transform;
+            }
+        }
+
+        IEnumerator FinishCombatViewFromPlayer()
+        {
+            yield return new WaitForSeconds((float)playableDirectors[3].duration);
+            playableDirectors[3].Stop();
+
+            if (playableDirectors[3].state != PlayState.Playing)
+            {
+                playableDirectors[0].Play();
+            }
+        }
+
+        IEnumerator FinishPlayerViewFromCombat()
+        {
+            yield return new WaitForSeconds((float)playableDirectors[2].duration);
+            playableDirectors[2].Stop();
+
+            if (playableDirectors[2].state != PlayState.Playing)
+            {
+                playableDirectors[1].Play();
+            }
         }
     }
 }
