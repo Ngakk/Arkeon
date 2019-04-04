a2d04a26d5a26e1eaa4b012e65120fbc54d092c7
agustin <villanuevaagustin77@gmail.com>
2019-04-04 11:32:56 -0600

    Update teams

--- Assets/_game/Scripts/Equipos/TeamManager.cs
+++ Assets/_game/Scripts/Equipos/TeamManager.cs
@@ -1,75 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeamManager : MonoBehaviour
+namespace Equipos
 {
-    // Libros
-    public List<GameObject> LibroLuz = new List<GameObject>();
-    public List<GameObject> LibroOscuridad = new List<GameObject>();
-    public List<GameObject> LibroTierra = new List<GameObject>();
-   
-    // Pokemon que siempre esta con el jugador
-    // Este wey no ocupa espacio en los libros
-    public GameObject Familiar;
+    public class TeamManager : MonoBehaviour
+    {
+        // Libros
+        public List<GameObject> LibroLuz = new List<GameObject>();
+        public int PokeLuz;
 
-    // Equipo para la batalla
-    private List<GameObject> EquipoCombate = new List<GameObject>();
+        public List<GameObject> LibroOscuridad = new List<GameObject>();
+        public int PokeOscuro;
 
-    public Camera cam;
-    public LayerMask mascara;
+        public List<GameObject> LibroTierra = new List<GameObject>();
+        public int PokeTierra;
+
+        // Pokemon que siempre esta con el jugador
+        // Este wey no ocupa espacio en los libros
+        public GameObject Familiar;
+
+        // Equipo para la batalla
+        public List<GameObject> EquipoCombate = new List<GameObject>();
+
+        public Camera cam;
+        public LayerMask mascara;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        LibroLuz.Capacity = 3;
-        LibroOscuridad.Capacity = 3;
-        LibroTierra.Capacity = 3;
-
-        EquipoCombate.Capacity = 9;
-        
-        cam = Camera.main;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        void Start()
         {
-            RaycastHit hit;
+            LibroLuz.Capacity = PokeLuz;
+            LibroOscuridad.Capacity = PokeOscuro;
+            LibroTierra.Capacity = PokeTierra;
 
-            // Linea invisible que se lanzara en la posicion donde tocamos la pantalla
-            Ray rayo = cam.ScreenPointToRay(Input.mousePosition);
+            EquipoCombate.Capacity = 9;
 
-            // Si el rayo colisiona con el objeto con el layer indicado
-            if (Physics.Raycast(rayo, out hit, Mathf.Infinity, mascara))
+            cam = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                Debug.Log(hit.collider);
+                RaycastHit hit;
 
-                if (LibroLuz.Count < 3)
+                // Linea invisible que se lanzara en la posicion donde tocamos la pantalla
+                Ray rayo = cam.ScreenPointToRay(Input.mousePosition);
+
+                // Si el rayo colisiona con el objeto con el layer indicado
+                if (Physics.Raycast(rayo, out hit, Mathf.Infinity, mascara))
                 {
-                    LibroLuz.Add(hit.collider.gameObject);
-                    Debug.Log(LibroLuz);
+                    if (LibroLuz.Count < LibroLuz.Capacity)
+                    {
+                        LibroLuz.Add(hit.collider.gameObject);
+                        EquipoCombate.Add(hit.collider.gameObject);
+                        hit.collider.gameObject.SetActive(false);
+                    }
                 }
             }
-        }
-    }
-
-    public void AgregarPoke(string NombreLibro)
-    {
-        if (NombreLibro == "Luz")
-        {
-            LibroLuz.Add(Familiar);
-        }
-        
-        if (NombreLibro == "Oscuridad")
-        {
-            LibroOscuridad.Add(Familiar);
-        }
-        
-        if (NombreLibro == "Tierra")
-        {
-            LibroTierra.Add(Familiar);
         }
     }
 }
---
+++ Assets/_game/Scripts/Equipos/Teams.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Equipos;
+
+public class Teams : MonoBehaviour
+{
+    public TeamManager manager;
+    
+    public List<Transform> Positions = new List<Transform>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MostrarLuz()
+    {
+        for (int i = 0; i < manager.LibroLuz.Count; i++)
+        {
+            //Instantiate(manager.LibroLuz[i], Positions[i].position, Quaternion.identity);
+            manager.LibroLuz[i].SetActive(true);
+            manager.LibroLuz[i].transform.position = Positions[i].position;
+        }
+    }
+}
