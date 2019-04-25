a81aed89ee4c3f559f52d59e1560cd72501439d9
SorenGedeon <felipeortega94@gmail.com>
2019-04-25 14:26:14 -0500

    Acomo de pokimons

--- Assets/_game/Scripts/Equipos/Equipar.cs
+++ Assets/_game/Scripts/Equipos/Equipar.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Equipar : MonoBehaviour
 {
+    public GameObject pos1;
+    public GameObject pos2;
+    public GameObject pos3;
+
     // Start is called before the first frame update
     void Start()
     {
