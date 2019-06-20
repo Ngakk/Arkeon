35ece9523c10235b0d44182c47d89b4b6ba870a5
Retus001 <retus001@gmail.com>
2019-06-20 15:04:07 -0500

    Info Panels Update

---
+++ Assets/_game/Battle_UI/Battle_UI_2/Scripts/BattleMenu_ArkeonInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BattleMenu_ArkeonInfo : MonoBehaviour
+{
+    public TextMeshProUGUI name;
+    public TextMeshProUGUI description;
+    public Image image;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
