5a13e3a69186124c37e19f68e5d57822583be8a7
Ngakk <32682022+Ngakk@users.noreply.github.com>
2019-07-23 14:52:30 -0500

    Cosas de bd

--- Assets/_game/Protoype Combat Gameplay/Scripts/Scriptable Objects/ArkeonData.cs
+++ Assets/_game/Protoype Combat Gameplay/Scripts/Scriptable Objects/ArkeonData.cs
@@ -34,5 +34,14 @@ namespace ArkeonBattle
         {
             return new List<ArkeonAttack>();
         }
+
+        public void GetBaseStatsFromDB()
+        {
+            MonsterBase temp = DBAccess.LoadArkeon(db_id);
+            baseStats = temp.baseStats;
+            originalName = temp.name;
+            DBAccess. //TODO: hacer que busque los tipos, luego que se pueda guardar a la BD desde el editor de este script
+        }
+
     }
 }
